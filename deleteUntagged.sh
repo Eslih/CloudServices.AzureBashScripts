f819e11cd88790cb0f5615baf53182b465dac31b
Esli Heyvaert <esli.heyvaert@gmail.com>
2020-09-23 12:39:25 +0200

    Delete untagged resources

--- deleteUntagged.sh
+++ deleteUntagged.sh
@@ -2,9 +2,16 @@
 
 TAG="student"
 
-RESOURCE_NAMES=$(az resource list --query "[?!(contains(to_string(tags), '$TAG'))].{ name: name }" -o tsv)
+RESOURCE_NAMES=$(az resource list --query "[?!(contains(to_string(tags), '$TAG'))].{ name: name, type: type }" --output json | jq -c '.[]')
+RESOURCE_GROUP="CloudServices"
+SUBSCRIPTION="MCT STUDENT"
 
+az account set --subscription "$SUBSCRIPTION"
 
 for RESOURCE_NAME in $RESOURCE_NAMES; do
   echo "Deleting $RESOURCE_NAME ..."
+  az resource delete \
+    --name "$(echo "$RESOURCE_NAME" | jq -r '.name')" \
+    --resource-type "$(echo "$RESOURCE_NAME" | jq -r '.type')" \
+    --resource-group "$RESOURCE_GROUP"
 done
